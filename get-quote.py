b68b104e347fe138e9fdc1a6ca7e1c00c6a49f74
Riyadul Islam Mollick <riyadulislammollickriyad@gmail.com>
2020-12-24 23:39:34 +0600

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
-def main:
+import random
+def main():
   print("Hello World")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  last = 13
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
-if __name__== "__main_":
+if __name__== "__main__":
   main()
